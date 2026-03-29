af7972606805ea99226f0d4600380171897f95c3
Gerard Cada <256761544+grd23cd@users.noreply.github.com>
2026-03-29 15:30:46 +0800

    Make image larger on hover

--- Online Voting System/voting management system/votesystem/votesystem/includes/navbar.php
+++ Online Voting System/voting management system/votesystem/votesystem/includes/navbar.php
@@ -1,16 +1,20 @@
-<header class="main-header" style="background-color:#717A83 ">
-  <nav class="navbar navbar-static-top" style="background-color:#717A83 ">
-    <div class="container" style="background-color:#717A83 ">
-      <div class="navbar-header" style="background-color:#717A83 ">
-        <a href="#" class="navbar-brand" style="background-color:#717A83 ;color:black ; font-size: 22px; font-family:Times  "><b>ONLINE<b> VOTING</b> SYSTEM</a>
-        <button type="button" class="navbar-toggle collapsed"style="background-color:#717A83 " data-toggle="collapse" data-target="#navbar-collapse">
+<header class="main-header">
+  <nav class="navbar navbar-static-top" style="background-color:#717A83; width:100%;">
+    <div style="display: flex; align-items: center; justify-content: space-between; width:100%; padding: 5px 20px; background-color:#717A83;">
+
+      <!-- Left: Brand -->
+      <div class="navbar-header" style="display: flex; align-items: center;">
+        <a href="#" class="navbar-brand" style="color:black; font-size: 22px; font-family: Times; white-space: nowrap;">
+          <b>ONLINE VOTING SYSTEM</b>
+        </a>
+        <button type="button" class="navbar-toggle collapsed" style="background-color:#717A83; margin-left: 10px;" data-toggle="collapse" data-target="#navbar-collapse">
           <i class="fa fa-bars"></i>
         </button>
       </div>
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
-        <ul class="nav navbar-nav">
+      <!-- Center: Nav Links -->
+      <div class="collapse navbar-collapse pull-left" id="navbar-collapse" style="display: flex; justify-content: center;">
+        <ul class="nav navbar-nav" style="display: flex; gap: 20px;">
           <?php
             if(isset($_SESSION['student'])){
               echo "
@@ -21,21 +25,83 @@
           ?>
         </ul>
       </div>
-      <!-- /.navbar-collapse -->
-      <!-- Navbar Right Menu -->
-      <div class="navbar-custom-menu"  >
-        <ul class="nav navbar-nav"  >
-          <li class="user user-menu" >
-            <a href="">
-              <img src="<?php echo (!empty($voter['photo'])) ? 'images/'.$voter['photo'] : 'images/profile.jpg' ?>" class="user-image" alt="User Image">
-              <span class="hidden-xs"  style="color:black ; font-size: 22px; font-family:Times  "><?php echo $voter['firstname'].' '.$voter['lastname']; ?></span>
+
+      <!-- Right: User Menu -->
+      <div class="navbar-custom-menu" style="display: flex; align-items: center; gap: 15px;">
+        <ul class="nav navbar-nav" style="display: flex; align-items: center; gap: 15px;">
+          <li class="user user-menu">
+            <a href="#" style="display: flex; align-items: center; gap: 10px;">
+              <div class="user-image-wrapper">
+                <img src="<?php echo (!empty($voter['photo'])) ? 'images/'.$voter['photo'] : 'images/profile.jpg'; ?>" 
+                     class="user-image" alt="User Image">
+              </div>
+              <span class="hidden-xs" style="color:black; font-size: 22px; font-family: Times; white-space: nowrap;">
+                <?php echo $voter['firstname'].' '.$voter['lastname']; ?>
+              </span>
             </a>
           </li>
-          <li><a href="logout.php"> <b style="color:black ; font-size: 22px;> <i class=fa fa-sign-out"> </b></i> <b style="color:black ; font-size: 22px; font-family:Times  " > LOGOUT </b></a></li>  
+          <li>
+            <a href="logout.php" style="display: flex; align-items: center; gap: 5px;">
+              <i class="fa fa-sign-out" style="color:black; font-size:22px;"></i>
+              <b style="color:black; font-size: 22px; font-family: Times; white-space: nowrap;"> LOGOUT </b>
+            </a>
+          </li>  
         </ul>
       </div>
-      <!-- /.navbar-custom-menu -->
     </div>
-    <!-- /.container-fluid -->
   </nav>
+
+  <!-- Hover effect CSS -->
+  <style>
+    body {
+      margin: 0;
+      background-color: #717A83;
+    }
+
+    /* Navbar adjusts dynamically */
+    .navbar {
+      background-color: #717A83;
+      display: flex;
+      align-items: center;
+      transition: all 0.3s ease;
+      min-height: 100px; /* enough for hover image growth */
+    }
+
+    /* Image wrapper */
+    .user-image-wrapper {
+      display: inline-block;
+      transition: all 0.3s ease;
+    }
+
+    /* Base user image size */
+    .user-image {
+      width: 80px !important;
+      height: 80px !important;
+      border-radius: 50%;
+      transition: all 0.3s ease;
+      display: block;
+    }
+
+    /* Hover effect */
+    .user.user-menu:hover .user-image-wrapper {
+      width: 160px;
+      height: 160px;
+    }
+
+    .user.user-menu:hover .user-image {
+      width: 160px !important;
+      height: 160px !important;
+    }
+
+    /* Vertically center text next to image */
+    .user.user-menu a {
+      display: flex;
+      align-items: center;
+    }
+
+    /* Override Bootstrap navbar img constraints */
+    .navbar-nav > li.user-menu > a img {
+      max-height: none !important;
+    }
+  </style>
 </header>
